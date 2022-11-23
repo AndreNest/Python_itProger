95b3f14c9ba63c8c35de4394d7736b61d97faec0
AndreyNesterov <nest.ande@yandex.ru>
2022-11-24 02:49:37 +0300

    complite lesson_05.py

--- lesson_ITProger.py
+++ lesson_ITProger.py
@@ -13,18 +13,34 @@
 #
 # print(f'Count = {count}')
 
-i = 5
-while i <= 15:
-    print(i)
-    i += 2
+# i = 5
+# while i <= 15:
+#     print(i)
+#     i += 2
+#
+# is_has_car = True
+# i = 5
+# while is_has_car:
+#     print(i)
+#     i += 2
+#
+# is_has_car = True
+# while is_has_car:
+#     if input('Введите стоп слово: ') == 'Stop':
+#         is_has_car = False
 
-is_has_car = True
-i = 5
-while is_has_car:
-    print(i)
-    i += 2
-
-is_has_car = True
-while is_has_car:
-    if input('Введите стоп слово: ') == 'Stop':
-        is_has_car = False
+# for i in range(1, 11):
+#     if i == 5:
+#         break
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+# found = None
+# for i in 'Hallow!':
+#     if i == 'r':
+#         found = True
+#         break
+# else:
+#     found = False
+#
+# print(found)
