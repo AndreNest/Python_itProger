f710428abaf72ecdf29fc95a065e3b0e11a0a6f7
AndreyNesterov <nest.ande@yandex.ru>
2022-11-24 02:39:54 +0300

    add while and for

--- lesson_ITProger.py
+++ lesson_ITProger.py
@@ -1,2 +1,30 @@
 # for i in range(1, 6, 2):
 #     print(i)
+
+# word = 'Hallow World!'
+#
+# for i in word:
+#     print(i*3)
+# count = 0
+# word = 'Hallow World!'
+# for i in word:
+#     if i == 'l':
+#         count += 1
+#
+# print(f'Count = {count}')
+
+i = 5
+while i <= 15:
+    print(i)
+    i += 2
+
+is_has_car = True
+i = 5
+while is_has_car:
+    print(i)
+    i += 2
+
+is_has_car = True
+while is_has_car:
+    if input('Введите стоп слово: ') == 'Stop':
+        is_has_car = False
