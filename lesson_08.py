0dc0070f163c24c21d057d72a79d0600933f8511
AndreyNesterov <nest.ande@yandex.ru>
2022-11-24 03:39:53 +0300

    add lesson_08

---
+++ lesson_08.py
@@ -0,0 +1,11 @@
+word = 'hallow world!'
+print(word[5]) # w
+print(len(word)) #13
+print(word.count('l')) # 3
+print(word.upper()) # HALLOW WORLD!
+print(word.isupper()) #False
+word = 'HALLOW WORLD!'
+print(word.isupper()) #True
+word = 'hallow world!'
+print(word.islower())
+print(word.capitalize())
