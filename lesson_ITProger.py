02adad4842e3497860d72f0162e7e370df841016
AndreyNesterov <nest.ande@yandex.ru>
2022-11-24 03:29:35 +0300

    add lesson_8

--- lesson_ITProger.py
+++ lesson_ITProger.py
@@ -44,3 +44,57 @@
 #     found = False
 #
 # print(found)
+
+
+# nums = [1,2,3,4,5, True, 'Hallow']
+# nums[0] = 50
+# nums[5] = 333
+# print(nums)
+# print(nums[6])
+# ##################################
+# nums = [1,2,3,4,5, True, 'Hallow', [1,2,3,4,'Privet']]
+# nums[0] = 50
+# nums[5] = 333
+# print(nums)
+# print(nums[6])
+# print(nums[7])
+# print(nums[-1]) #Последний элемент
+# print(nums[7][4])
+
+# numbers = [5, 2, 7]
+# # numbers[3] = 100
+# numbers.append(100) # [5, 2, 7, 100] добовляет в конце
+# numbers.insert(1, True) #[5, True, 2, 7, 100] добовляет в нужном месте
+# numbers.extend([5,6,7])# [5, True, 2, 7, 100, 5, 6, 7]  добовляет список к списку
+# b = [3, 222, 555]
+# numbers.extend(b) # [5, True, 2, 7, 100, 5, 6, 7, 3, 222, 555] добовляет список к списку
+#
+# numbers.sort() # [True, 2, 3, 5, 5, 6, 7, 7, 100, 222, 555] сортирует
+# # numbers.reverse() # [555, 222, 100, 7, 7, 6, 5, 5, 3, 2, True] разворачивает
+#
+# numbers.pop() # [True, 2, 3, 5, 5, 6, 7, 7, 100, 222] удаляет последний элемент
+# numbers.pop(8) # [2, 3, 5, 5, 6, 7, 7, 222] удаляет элемент по индексу
+# numbers.remove(True)#  [2, 3, 5, 5, 6, 7, 7, 100, 222] удаляет конкретное значнение
+#
+# # numbers.clear() # [] очищает список
+#
+#
+#
+# print(numbers.count(222)) # считает количество найденых элементов
+# print(len(numbers)) #длина списка
+
+# nums = [5, 2, 7, '50', False]
+#
+# for element in nums:
+#     element *= 2
+#     print(element)
+
+lens = int(input('Enter lens: '))
+i = 0
+arr = []
+while i < lens:
+    string = 'Enter ele,ent #' + str(i + 1) +  ':'
+    arr.append(input(string))
+    i += 1
+
+print(arr)
